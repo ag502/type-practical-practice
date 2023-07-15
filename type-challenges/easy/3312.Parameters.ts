acb7d52db3bf942ae73714c77919aa60746665c6
ag502 <ag502@naver.com>
2023-07-15 23:35:36 +0900

    add: type challenge 3312.Parameters

---
+++ type-challenges/easy/3312.Parameters.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* _____________ Your Code Here _____________ */
+
+type MyParameters<T extends (...args: any[]) => any> = T extends (...args: infer A) => any ? A : never;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+const foo = (arg1: string, arg2: number): void => {};
+const bar = (arg1: boolean, arg2: { a: "A" }): void => {};
+const baz = (): void => {};
+
+type cases = [
+  Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
+  Expect<Equal<MyParameters<typeof bar>, [boolean, { a: "A" }]>>,
+  Expect<Equal<MyParameters<typeof baz>, []>>
+];
